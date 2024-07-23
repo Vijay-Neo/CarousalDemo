18bfe747490b810be0935652f5763935e8ba5701
Vijay-Neo <vijay.raval@neosoftmail.com>
2024-07-23 16:19:57 +0530

    code cleanup

--- app/src/main/java/com/example/demo/utils/CommonUtils.kt
+++ app/src/main/java/com/example/demo/utils/CommonUtils.kt
@@ -2,31 +2,30 @@ package com.example.demo.utils
 
 import android.app.Activity
 import android.view.View
-import android.widget.TextView
 import com.example.demo.R
+import com.example.demo.databinding.BottomsheetDialogBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 object CommonUtils {
 
     fun showBottomSheetDialog(
         context: Activity,
-        itemCount: Int?=0,
-        top3Chars: List<MutableMap.MutableEntry<Char, Int>>?) {
-        val dialogView = context.layoutInflater.inflate(R.layout.bottomsheet_dialog, null)
-        val itemCountTextView = dialogView.findViewById<TextView>(R.id.itemCountTextView)
-        val topCharactersTextView = dialogView.findViewById<TextView>(R.id.topCharactersTextView)
+        itemCount: Int? = 0,
+        top3Chars: List<MutableMap.MutableEntry<Char, Int>>? = null
+    ) {
+        val dialogBinding = BottomsheetDialogBinding.inflate(context.layoutInflater)
 
-        if(itemCount != 0) {
+        if (itemCount != 0) {
             val topCharacters = top3Chars?.joinToString("\n") { "${it.key} = ${it.value}" }
-            itemCountTextView.text = context.getString(R.string.item_count, itemCount)
-            topCharactersTextView.text = context.getString(R.string.top_characters, topCharacters)
-        }else{
-            itemCountTextView.text = context.getString(R.string.no_data)
-            topCharactersTextView.visibility =  View.INVISIBLE
+            dialogBinding.itemCountTextView.text = context.getString(R.string.item_count, itemCount)
+            dialogBinding.topCharactersTextView.text = context.getString(R.string.top_characters, topCharacters)
+        } else {
+            dialogBinding.itemCountTextView.text = context.getString(R.string.no_data)
+            dialogBinding.topCharactersTextView.visibility = View.INVISIBLE
         }
 
         val bottomSheetDialog = BottomSheetDialog(context)
-        bottomSheetDialog.setContentView(dialogView)
+        bottomSheetDialog.setContentView(dialogBinding.root)
         bottomSheetDialog.show()
     }
 }
